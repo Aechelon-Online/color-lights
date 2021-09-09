48eb24782a3a4fd3e414269e45305247aa490778
Doug Luce <doug@github.con.com>
2021-09-09 11:52:03 -0700

    Tidy things up a bit.
    Loop over what we need.

--- script.js
+++ script.js
@@ -1,89 +1,33 @@
-const red = document.querySelector('.red')
-const limegreen = document.querySelector('.limegreen')
-const blue = document.querySelector('.blue')
-const yellow = document.querySelector('.yellow')
-const cyan = document.querySelector('.cyan')
-const magenta = document.querySelector('.magenta')
-const green = document.querySelector('.green')
-const orange = document.querySelector('.orange')
-const purple = document.querySelector('.purple')
-let time = "1s"
+// Human-manageable map of keystrokes to colors.
 
-document.addEventListener("keypress", (e) => {
-    if (e.repeat) return
-    if (e.key === "1") {
-        red.classList.remove("fade")
-        red.classList.add("show")
-    }
-    if (e.key === "2") {
-        limegreen.classList.remove("fade")
-        limegreen.classList.add("show")
-    }
-    if (e.key === "3") {
-        blue.classList.remove("fade")
-        blue.classList.add("show")
-    }
-    if (e.key === "4") {
-        yellow.classList.remove("fade")
-        yellow.classList.add("show")
-    }
-    if (e.key === "5") {
-        cyan.classList.remove("fade")
-        cyan.classList.add("show")
-    }
-    if (e.key === "6") {
-        magenta.classList.remove("fade")
-        magenta.classList.add("show")
-    }
-    if (e.key === "7") {
-        orange.classList.remove("fade")
-        orange.classList.add("show")
-    }
-    if (e.key === "8") {
-        purple.classList.remove("fade")
-        purple.classList.add("show")
-    }
-    if (e.key === "9") {
-        green.classList.remove("fade")
-        green.classList.add("show")
-    }
+const colormap = {
+  1: 'red',
+  2: 'limegreen',
+  3: 'blue',
+  4: 'yellow',
+  5: 'cyan',
+  6: 'magenta',
+  7: 'orange',
+  8: 'purple',
+  9: 'green'
+}
+
+// Build a map of keystrokes to corresponding color DOM elements.
+const elements = {}
+for (const [key, value] of Object.entries(colormap)) {
+  elements[key] = document.querySelector(`.${value}`)
+}
+
+// Add listeners.
+document.addEventListener("keypress", e => {
+  if (e.repeat || !(e.key in elements)) return
+  elements[e.key].classList.remove("fade")
+  elements[e.key].classList.add("show")
 })
 
-document.addEventListener("keyup", (e) => {
-    if (e.key === "1") {
-        red.classList.add("fade")
-        red.classList.remove("show")
-    }
-    if (e.key === "2") {
-        limegreen.classList.add("fade")
-        limegreen.classList.remove("show")
-    }
-    if (e.key === "3") {
-        blue.classList.add("fade")
-        blue.classList.remove("show")
-    }
-    if (e.key === "4") {
-        yellow.classList.add("fade")
-        yellow.classList.remove("show")
-    }
-    if (e.key === "5") {
-        cyan.classList.add("fade")
-        cyan.classList.remove("show")
-    }
-    if (e.key === "6") {
-        magenta.classList.add("fade")
-        magenta.classList.remove("show")
-    }
-    if (e.key === "7") {
-        orange.classList.add("fade")
-        orange.classList.remove("show")
-    }
-    if (e.key === "8") {
-        purple.classList.add("fade")
-        purple.classList.remove("show")
-    }
-    if (e.key === "9") {
-        green.classList.add("fade")
-        green.classList.remove("show")
-    }
+document.addEventListener("keyup", e => {
+  if (!(e.key in elements)) return
+  elements[e.key].classList.add("fade")
+  elements[e.key].classList.remove("show")
 })
+
